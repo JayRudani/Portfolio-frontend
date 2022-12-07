b7fad7029159181fdafbb085c4d86f75697b39c6
JayRudani <jayrudani1414@gmail.com>
2022-12-07 17:04:31 -0500

    Front-end Complete

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+// import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import linkedInIcon from "../assets/img/nav-icon1.svg";
+import githubIcon from '../assets/img/github-mark-white.svg'
+import resumeIcon from '../assets/img/resume.png'
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/jayrudani98/" target="_blank"  rel="noopener noreferrer"><img src={linkedInIcon} alt="Icon" /></a>
+              <a href="https://github.com/JayRudani" target="_blank"  rel="noopener noreferrer"><img src={githubIcon} alt="Icon" /></a>
+              <a href="https://drive.google.com/file/d/1_Xbm5Dk3g21XSM_HJPUzRgvc8yiTFm97/view?usp=share_link" target="_blank"  rel="noopener noreferrer"><img src={resumeIcon} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
