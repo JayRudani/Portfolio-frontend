2830af0a91a3f53c12a2e3fefbfd3a58d74d3ebe
JayRudani <jayrudani1414@gmail.com>
2022-12-08 13:31:12 -0500

    POST link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://jay-rudani-portfolio-be.netlify.app/", {
+    let response = await fetch("https://netlify-express.netlify.com/.netlify/functions/server/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
