8a0359438edd5705b7fe2d8b50926879586ebc64
JayRudani <jayrudani1414@gmail.com>
2024-04-01 18:40:43 -0400

    Resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export const Footer = () => {
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/jayrudani98/" target="_blank"  rel="noopener noreferrer"><img src={linkedInIcon} alt="Icon" /></a>
               <a href="https://github.com/JayRudani" target="_blank"  rel="noopener noreferrer"><img src={githubIcon} alt="Icon" /></a>
-              <a href="https://drive.google.com/file/d/1j2bP8TTfpzSvXxcLcl6xv4G782cuyPmW/view?usp=sharing" target="_blank"  rel="noopener noreferrer"><img src={resumeIcon} alt="Icon" /></a>
+              <a href="https://drive.google.com/file/d/1MY9rpP8PKaDHeIkzkjXtsm0Ih6Wld218/view?usp=sharing" target="_blank"  rel="noopener noreferrer"><img src={resumeIcon} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
